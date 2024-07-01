ad130c0c8836c344d1f39e5e85c95dc738e997e9
Nikolas Arruda Caracho <nikolasarruda762@gmail.com>
2024-07-01 10:50:08 -0300

    Condicionais e concatenação

--- Projeto 01 (Número secreto)/logica-js-projeto_inicial/app.js
+++ Projeto 01 (Número secreto)/logica-js-projeto_inicial/app.js
@@ -7,15 +7,21 @@ alert("Boas vindas ao jogo do número secreto!");
 // caso outra pessoa leia seu código, presuma o que ela faz
 //shift + atlt + A para comentários em bloco
 
-let numeroSecreto = 29;
+let numeroSecreto = 5;
+// colocar sem aspas ja que vc deseja exibir o valor da variavel em si
+console.log(numeroSecreto);
 //scanf: msg na tela + receber variável
 //lembrar de testar os erros!
 let chute = prompt("Escolha um número entre 1 e 10");
+//transformar o prompt em variável
 
 //iniciando a lógica para testar se o número
 //recebido em prompt é igual ao número secreto predeterminado
 //Ao invés de usar o operador de atribuição "=", que atribui o valor da variável "numeroSecreto" à variável "chute", você deve usar o operador de comparação "==", que verifica se os valores das duas variáveis são iguais.
 if (chute == numeroSecreto) {
-  console.log("Isso aí! Você descobriu o número secreto (numeroSecreto)");
+  //para fazer a concatenação de strings e variáveis vc deve substituir as aspas por crase ` e inserir a variavel dentro de cifrao e chaves
+  // console.log(`Texto ${variavel}`);
+  alert(`Parabéns, você acertou o número secreto! (${numeroSecreto})`);
+} else {
+  alert(`Você errou! O número secreto era: (${numeroSecreto})`);
 }
-//transformar o prompt em variável
