14424cd024ab743c77a4fb40d0f41f7e434a5b02
Nikolas Arruda Caracho <nikolasarruda762@gmail.com>
2024-07-01 09:24:31 -0300

    Prompt do número

--- Projeto 01 (Número secreto)/logica-js-projeto_inicial/app.js
+++ Projeto 01 (Número secreto)/logica-js-projeto_inicial/app.js
@@ -1,3 +1,13 @@
 //inserindo uma mensagem na tela.
-//pode ser em aspas simples por ser js, mas lembre-se de padronizar durante todo o projeto
+//pode ser em aspas simples por ser JS, mas lembre-se de padronizar durante todo o projeto
 alert ('Boas vindas ao jogo do número secreto!');
+
+/* //criando variáveis no JS
+//nomear de forma intuitiva a ponto de,
+// caso outra pessoa leia seu código, presuma o que ela faz */
+//shift + atlt + A para comentários em bloco
+let numeroSecreto = 29;
+
+//scanf: msg na tela + receber variável
+//lembrar de testar os erros!
+prompt ('Escolha um número entre 1 e 10');
