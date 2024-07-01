891a1d2e992af3410f693cf71c45c71de595b7c7
Nikolas Arruda Caracho <nikolasarruda762@gmail.com>
2024-07-01 11:43:01 -0300

    Update app.js

--- Projeto 01 (Número secreto)/Desafio 02/app.js
+++ Projeto 01 (Número secreto)/Desafio 02/app.js
@@ -1,35 +1,35 @@
 //1-Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".
 
-let diaSemana = prompt('Qual o dia da semana?');
-if (diaSemana == 'Sábado' || diaSemana == 'Domingo'){
-    alert('Bom fim de semana!');
+let diaSemana = prompt("Qual o dia da semana?");
+if (diaSemana == "Sábado" || diaSemana == "Domingo") {
+  alert("Bom fim de semana!");
 } else {
-    alert ('Boa semana!');
+  alert("Boa semana!");
 }
 
 /* 2-Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando. */
 
-let numero = prompt ('Digite um número');
-if(numero >= 0){
-    alert('Número positivo');
+let numero = prompt("Digite um número");
+if (numero >= 0) {
+  alert("Número positivo");
 } else {
-    alert ('Número negativo');
+  alert("Número negativo");
 }
 
 /* 3-Crie um sistema de pontuação para um jogo. Se a pontuação for maior ou igual a 100, mostre "Parabéns, você venceu!". Caso contrário, mostre "Tente novamente para ganhar.". */
 
-let pontuacao = prompt ('Insira sua pontuação:');
-if (pontuacao >= 100){
-    alert('Parabeéns, você venceu!');
+let pontuacao = prompt("Insira sua pontuação:");
+if (pontuacao >= 100) {
+  console.log("Parabéns, você venceu!");
 } else {
-    alert ('Tente novamente para ganhar.');
+  console.log("Tente novamente para ganhar.");
 }
 
 /* 4-Crie uma mensagem que informa o usuário sobre o saldo da conta, usando uma template string para incluir o valor do saldo.
  */
 let saldo = 500;
-alert (`Saldo: R$${saldo}`);
+alert(`Saldo: R$${saldo}`);
 
 //5-Peça ao usuário para inserir seu nome usando prompt. Em seguida, mostre um alerta de boas-vindas usando esse nome.
-let nome = prompt ('Insira seu nome');
-alert(`Boas vindas, ${nome}!`)
+let nome = prompt("Insira seu nome");
+alert(`Boas vindas, ${nome}!`);
