c90817f0b96eee34c7341650cd8734f529003c4d
Nikolas Arruda Caracho <nikolasarruda762@gmail.com>
2024-07-02 09:17:28 -0300

    Aula 1
    document,querySelector('NomeDaTag');
    variável.innerHTML='texto';

---
+++ curso_02_/js-curso2-aula_1/app.js
@@ -0,0 +1,8 @@
+//document seleciona o html
+//querySelector('NomeDaTag'); seleciona a tag
+let titulo = document.querySelector('h1')
+//seleciona a variável titulo e escolhe alterar dentro do html 
+titulo.innerHTML = 'Jogo do número secreto';
+
+let paragrago = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
