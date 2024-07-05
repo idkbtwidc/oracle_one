ab846aba727cdae29feb29a233e6b186c05d7052
Nikolas Arruda Caracho <nikolasarruda762@gmail.com>
2024-07-05 10:40:00 -0300

    Imc

--- curso_02_/Desafio 03/app.js
+++ curso_02_/Desafio 03/app.js
@@ -1,22 +1,27 @@
 //1-Crie uma função que calcule o índice de massa corporal (IMC) de uma pessoa, a partir de sua altura, em metros, e peso, em quilogramas, que serão recebidos como parâmetro.
+//1-definição do valor das variáveis
+//2-função
+//3- return pra variável
+
+//definição de variáveis usando desestruturação de "arrays"
+
+let [altura, peso] = [1.75, 65];
+
+let imc = calculoImc(altura, peso);
+
+console.log(`Seu IMC é: ${imc}`);
+
+//Lembrar da definição do Imc fora da função
+function calculoImc(altura, peso) { // a função aceita dois parâmetros: altura e peso.
+  let imc = peso / (altura ** 2);
+  return imc;
+
+}
 
 //Testando "Arrow Function"
 //let calculoImc = (altura,peso) => (altura * altura)/peso;
 
-let altura = 1.75;
-let peso = 65;
-let resultadoImc = calculoImc(altura, peso);
-console.log(`Seu IMC é: ${resultadoImc}`);
-
-//Lembrar da definição do resultadoImc fora da função
-function calculoImc(altura, peso) {
-  let resultadoImc = (altura * altura)/peso;
-  return resultadoImc;
-}
-
-
 //2-Criar uma função que calcule o valor do fatorial de um número passado como parâmetro.
 let numero = 4;
-function fatorial (numero) {
-    factorialize 
-}
+function fatorial(numero) {
+  factorialize; }
